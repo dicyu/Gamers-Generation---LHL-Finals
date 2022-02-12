d31165af982092ac3054397b31730ea27f6d4eea
Dickson Yu <dickson.yu@email.com>
2022-02-12 15:28:30 -0500

    Adding closing modal with esc key using useEffect, changing props to by dynamic

--- gamers-generation/src/components/Modal.jsx
+++ gamers-generation/src/components/Modal.jsx
@@ -3,18 +3,27 @@ import './Modal.css';
 
 
 const ReportModal = props => {
-  if (!props.show) {
-    return null
+
+  const closeWhenEscapeKey = (e) => {
+    if ((e.charCode || e.keyCode) === 27) {
+      props.onClose()
+    }
   }
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', closeWhenEscapeKey)
+    return function cleanUp() {
+      document.body.removeEventListener('keydown', closeWhenEscapeKey)
+    }
+  }, [])
+
   return (
-    <div className='modal' onClick={props.onClose}>
+    <div className={`modal ${props.show ? 'show' : ''}`} onClick={props.onClose}>
       <div className='modal-content' onClick={e => e.stopPropagation()}>
         <div className='modal-header'>
-          <h4 className='modal-title'>Report</h4>
+          <h4 className='modal-title'>{props.title}</h4>
         </div>
-        <div className='modal-body'>
-          Content
-        </div>
+        <div className='modal-body'>{props.children}</div>
         <div className='modal-footer'>
           <button onClick={props.onClose} className='button'>Close</button>
         </div>
