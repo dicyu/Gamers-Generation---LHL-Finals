2f0fec63fd683200debdd7324c8a5a473aed22f9
Dickson Yu <dickson.yu@email.com>
2022-02-12 13:47:18 -0500

    Adding App

---
+++ gamers-generation/src/components/Application.jsx
@@ -0,0 +1,14 @@
+// Importing
+import React from 'react'
+import ReportModal from './Modal'
+
+const Application = function(props) {
+  return (
+    <div className="Report">
+      <button>Show Modal</button>
+      <ReportModal />
+    </div>
+  )
+}
+
+export default Application
