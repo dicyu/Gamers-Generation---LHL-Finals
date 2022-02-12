dcf7d36a25995d29d4ae9964b58f99f4482c22ed
Dickson Yu <dickson.yu@email.com>
2022-02-12 14:17:53 -0500

    Adding onclick and onclose and usestate to modal

--- gamers-generation/src/components/Application.jsx
+++ gamers-generation/src/components/Application.jsx
@@ -1,12 +1,14 @@
 // Importing
-import React from 'react'
+import React, { useState } from 'react'
 import ReportModal from './Modal'
 
 const Application = function(props) {
+  const [show, setShow] = useState(false)
+
   return (
     <div className="Report">
-      <button>Show Modal</button>
-      <ReportModal />
+      <button onClick={() => setShow(true) }>Report</button>
+      <ReportModal onClose={() => setShow(false)} show={show} />
     </div>
   )
 }
