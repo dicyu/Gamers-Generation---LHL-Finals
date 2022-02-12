698b231ce231aec11b5a99dd9983ce18f7366782
Dickson Yu <dickson.yu@email.com>
2022-02-12 16:46:59 -0500

    Adding comments

--- gamers-generation/src/components/Modal.jsx
+++ gamers-generation/src/components/Modal.jsx
@@ -1,15 +1,18 @@
+// Imports
 import React, { useEffect } from 'react'
 import './Modal.css';
 
 
 const ReportModal = props => {
 
+  // Function for Esc key use to close
   const closeWhenEscapeKey = (e) => {
     if ((e.charCode || e.keyCode) === 27) {
       props.onClose()
     }
   }
 
+  // Clean up for esc key
   useEffect(() => {
     document.body.addEventListener('keydown', closeWhenEscapeKey)
     return function cleanUp() {
@@ -17,6 +20,7 @@ const ReportModal = props => {
     }
   }, [])
 
+  // Render for modal
   return (
     <div className={`modal ${props.show ? 'show' : ''}`} onClick={props.onClose}>
       <div className='modal-content' onClick={e => e.stopPropagation()}>
