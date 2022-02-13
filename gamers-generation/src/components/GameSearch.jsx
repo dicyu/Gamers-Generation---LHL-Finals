e3f787bf1377174054d8c33c4949afd5c86e9f1a
Dickson Yu <dickson.yu@email.com>
2022-02-13 15:46:53 -0500

    Adding game-search files and some mock data for testing

---
+++ gamers-generation/src/components/GameSearch.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Data from './mock-data.json'
+
+import './GameSearch.scss'
+
+function GameSearch(props) {
+  
+  return (
+    <h1>Test</h1>
+  );
+}
+
+export default GameSearch;
