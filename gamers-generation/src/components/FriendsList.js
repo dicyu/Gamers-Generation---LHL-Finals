9788bfd8085b07ad531f22afc49b2f3555ae49ae
Cassie/Ren <cassandra.chen@uoit.net>
2022-02-12 22:16:46 -0500

    create components for list and list items for friends and games

--- gamers-generation/src/components/FriendsList.js
+++ gamers-generation/src/components/FriendsList.js
@@ -1,4 +1,14 @@
 import React from "react";
 import classNames from "classnames";
+import FriendsListItem from "./FriendsListItem";
 
-export default function FriendsList(props) {}
+export default function FriendsList(props) {
+  return (
+    <FriendsListItem
+      id={props.id}
+      selected={props.selected}
+      gamer_tag={props.gamer_tag}
+      timezone={props.timezone}
+    />
+  );
+}
--- gamers-generation/src/components/FriendsListItem.js
+++ gamers-generation/src/components/FriendsListItem.js
@@ -1,11 +1,27 @@
 import React from "react";
+import classNames from "classnames";
 
 export default function FriendsListItem(props) {
+  // props: gamer_tag, games_id, timezone
+
+  const selectFriend = classNames("friend", {
+    "friend--selected": props.selected,
+    "friend--gamer_tag": props.gamer_tag,
+  });
+
   return (
-    <button
-      src="images/rawr.png"
-      className="Add"
-      onSubmit={(event) => event.preventDefault}
-    ></button>
+    <section>
+      <li
+        onClick={() => props.onChange(props.gamers.id)}
+        className={selectFriend}
+      >
+        <h2>Gamer tag: {props.gamer_tag}</h2>
+        <h3>Timezone: {props.timezone}</h3>
+        {/* <h3>Status: {props.online}</h3> */}
+      </li>
+      {props.selected && props.gamer_tag}
+    </section>
   );
 }
+
+// work on what you'd want this to have later
