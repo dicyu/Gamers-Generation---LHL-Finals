f1c81c2773a13ea6f57971993fcead8798fcca3a
Dickson Yu <dickson.yu@email.com>
2022-02-13 16:14:43 -0500

    Adding rendering of the test data

--- gamers-generation/src/components/GameSearch.jsx
+++ gamers-generation/src/components/GameSearch.jsx
@@ -4,9 +4,19 @@ import Data from './mock-data.json'
 import './GameSearch.scss'
 
 function GameSearch(props) {
-  
+  const [query, setQuery] = useState("")
   return (
-    <h1>Test</h1>
+    <div className='game-search'>
+      <input className='search-bar' placeholder="Enter Post Title" onChange={e => setQuery(e.target.value)} />
+      {
+      Data.map((game, index) => (
+      <div className="box" key={index}>
+        <p>{game.name}</p>
+        <p>{game.released}</p>
+      </div>
+        ))
+      }
+    </div>
   );
 }
 
