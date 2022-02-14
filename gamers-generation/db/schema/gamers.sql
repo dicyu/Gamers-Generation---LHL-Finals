fc51e225acbcd98b7a50cf881fbdc2327957b286
Cassie/Ren <cassandra.chen@uoit.net>
2022-02-14 10:16:57 -0500

    wrong branch but scss for input field for user form complete, and edit db to reflect image changes

--- gamers-generation/db/schema/gamers.sql
+++ gamers-generation/db/schema/gamers.sql
@@ -4,6 +4,7 @@ CREATE TABLE gamers(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   gamer_tag VARCHAR(255),
+  avatar VARCHAR(255) NOT NULL,
   bio TEXT,
   email VARCHAR(255),
   password VARCHAR(255),
