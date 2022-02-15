71f4a57cb954030af14754b9e282ca047d3d3364
Dickson Yu <dickson.yu@email.com>
2022-02-14 21:34:19 -0500

    Changed how the search funciton is working

--- gamers-generation/src/components/GameSearch.jsx
+++ gamers-generation/src/components/GameSearch.jsx
@@ -1,29 +1,66 @@
 import React, { useState } from 'react'
-import Data from './mock-data.json'
+
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from '@mui/icons-material/Close';
 
 import './GameSearch.scss'
 
-function GameSearch(props) {
-  const [query, setQuery] = useState("")
+function GameSearch({ placeholder, data }) {
+  const [filteredGame, setFilteredGame] = useState([]);
+  const [query, setQuery] = useState('');
+
+  const gameFilter = (e) => {
+    const queryWord = e.target.value;
+    setQuery(queryWord);
+
+    const newGameSearched = data.filter((game) => {
+      return game.name.toLowerCase().includes(queryWord.toLowerCase());
+    });
+
+    if (queryWord === '') {
+      setFilteredGame([]);
+    } else {
+      setFilteredGame(newGameSearched);
+    }
+  };
+
+  const clearQuery = () => {
+    setFilteredGame([]);
+    setQuery('');
+  };
+
   return (
-    <div className='game-search'>
-      <input className='search-bar' placeholder="Enter Game Title" onChange={e => setQuery(e.target.value)} />
-      <br></br>
-      {
-        Data.filter(game => {
-          if (query === '') {
-            return null
-          } else if (game.name.toLowerCase().includes(query.toLowerCase())) {
-            return game;
-          }
-        }).map((game, index) => (
-          <div className="box" key={index}>
-            <p>{game.name} {game.released}</p>
-            </div>
-        ))
-      }
+    <div className='search'>
+      <div className='searchInputs'>
+        <input
+          type='text'
+          placeholder={placeholder}
+          value={query}
+          onChange={gameFilter}
+        />
+        <div className='searchIcon'>
+          {filteredGame.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id='clearBtn' onClick={clearQuery} />
+          )}
+        </div>
+      </div>
+      {filteredGame.length != 0 && (
+        <div className='dataResult'>
+          {filteredGame.slice(0, 15).map((value, key) => {
+            return (
+              <a className='dataItem' key={key} href={value.link} target='_blank'>
+                <p>{value.name}</p>
+              </a>
+            );
+          })}
+    </div>
+    )}
     </div>
   );
 }
+
+
 
 export default GameSearch;
