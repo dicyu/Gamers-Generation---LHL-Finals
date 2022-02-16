2c92b4b6f3ec53ae9e35a0872c9bcf4c856cc576
bybang <bybang26@gmail.com>
2022-02-16 08:43:34 -0500

    Implement swipeable tinder card, header icon and username

--- gamers-generation/src/App.js
+++ gamers-generation/src/App.js
@@ -1,10 +1,10 @@
+import React, { useState } from "react";
 import "./App.scss";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Sidebar from "./components/Sidebar";
-
-import React, { useState } from "react";
 import ReportModal from "./components/Modal";
+import ProfileCards from "./components/ProfileCards";
 
 function App() {
   const user = true;
@@ -16,13 +16,18 @@ function App() {
     <div className="App">
       <Navigation />
       <Header />
-      {user ? (
-        <div className="sidebar__container">
-          <Sidebar />
+      <div className="main__container">
+        {user ? (
+          <div className="sidebar__container">
+            <Sidebar />
+          </div>
+        ) : null}
+        {/* input */}
+        <div className="cards_container">
+          <ProfileCards />
+          <ProfileCards />
         </div>
-      ) : null}
-      {/* input */}
-
+      </div>
       {/* Modal Button Press (JUST A TEST BUTTON) */}
       {/* <div className="Report">
         <button onClick={() => setShow(true)}>Report</button>
