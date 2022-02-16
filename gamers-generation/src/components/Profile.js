c7f4e959b217f3b6ec2a38bd5b6d8dd14b754aef
Dickson Yu <dickson.yu@email.com>
2022-02-16 18:34:50 -0500

    Added some test data

--- gamers-generation/src/components/Profile.js
+++ gamers-generation/src/components/Profile.js
@@ -3,10 +3,27 @@ import React from 'react'
 import './Profile.scss'
 
 function Profile(props) {
+
+  const testData = ['Ren', 'bybang', 'pro.di.gy'];
+
   return (
-    <div className='bio__container'>
-      <p>blahblahblahblah</p>
-    </div>
+
+      <div className='bio__container'>
+        <div className='bio__recent-box'>
+          Recent Gamers <span class='bio__inner-text'>Played With</span>
+        </div>
+          {testData.map(gamer => (
+          <div className='gamers-container'>
+            <div className='gamers-name'>
+              <span className='profile-pic'>
+                Picture
+              </span>
+              {gamer}
+            </div>
+          </div>
+        ))}
+      </div>
+
   )
 }
 
