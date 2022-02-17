8a8f389a1f49ed3b7840ef1ac138cc37d132cd6e
Dickson Yu <dickson.yu@email.com>
2022-02-17 14:07:17 -0500

    Initial commit

---
+++ gamers-generation/src/components/Slideshow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SlideShow () {
+  return (
+    <div className='slideshow'>
+      <div className='slider'>
+        <div className='sliding'>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SlideShow;
