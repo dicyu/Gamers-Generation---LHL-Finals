6719adf444da4949bd42bf5707aaad9926aacc10
Dickson Yu <dickson.yu@email.com>
2022-02-19 17:58:21 -0500

    Adding body info and scss

---
+++ gamers-generation/src/components/Body.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Body.scss'
+
+function Body(props) {
+  return (
+    <div className='container'>
+      <div className='container__one'>
+        daslkda
+      </div>
+      <div className='container__two'>
+        dklasldsad
+      </div>
+      <div className='container__three'>
+        daklsdsmd
+      </div>
+    </div>
+  )
+}
+
+export default Body;
