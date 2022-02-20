6aca38fa465b882890d80734db45b3539e0fc7b3
Dickson Yu <dickson.yu@email.com>
2022-02-19 23:36:25 -0500

    Fixing some css with the containers

--- gamers-generation/src/components/Body.js
+++ gamers-generation/src/components/Body.js
@@ -5,17 +5,23 @@ function Body(props) {
   return (
     <div className='container'>
       <div className='container__one'>
-        <h2 className='container__header'>Matching</h2>
+        <h2 className='container__header'>
+          <div className='header__text'>Testing</div>
+        </h2>
         <p className='container__title'>Testing info here</p>
         <p>More Info</p>
       </div>
       <div className='container__two'>
-        <h2>Why use our platform</h2>
+        <h2 className='container__header'>
+        <div className='header__text'>Testing</div>
+        </h2>
         <p>Testing info here</p>
         <p>More Info</p>
       </div>
       <div className='container__three'>
-        <h2>Dunno</h2>
+        <h2 className='container__header'>
+        <div className='header__text'>Testing</div>
+        </h2>
         <p>Testing info here</p>
         <p>More Info</p>
       </div>
