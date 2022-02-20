f1680f8474884b83ef9b65d4fbadbf4f8692b713
Dickson Yu <dickson.yu@email.com>
2022-02-19 23:39:39 -0500

    Adding login route

--- gamers-generation/src/App.js
+++ gamers-generation/src/App.js
@@ -77,6 +77,7 @@ function App() {
             }
           />
           <Route path="/register" element={<Register />} />
+          <Route path='login' element={<Login />} />
         </Routes>
       </Router>
     </div>
