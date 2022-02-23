2fbd21276bddae98470846efc962027504f79292
Cassie/Ren <cassandra.chen@uoit.net>
2022-02-23 16:45:17 -0500

    profile card at the end tested

--- gamers-generation/src/stories/index.stories.js
+++ gamers-generation/src/stories/index.stories.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -6,6 +6,8 @@ import { action } from "@storybook/addon-actions";
 import Input from "../components/Information/Input";
 import Loading from "../components/Information/Loading";
 import Button from "../components/Button";
+import TinderCard from "react-tinder-card";
+import "../components/ProfileCards.scss"
 
 storiesOf("Input", module).add("Base", () => (
   <Input placeholder={"Death is upon us :D"} />
@@ -22,3 +24,29 @@ storiesOf("Button", module)
 
 storiesOf("Loading", module).add("Base", () => 
   <Loading />);
+
+storiesOf("Tinder Card", module).add("Out of Matches", () => 
+  <TinderCard
+        ref={useRef(0)}
+        className="swipe"
+        key={"Panda says No"}
+        preventSwipe={["up", "down", "left", "right"]}
+        >
+        <div className="card_bio__container">
+          <div
+            className="card"
+            style={{
+              backgroundImage: `url(https://tinyurl.com/2p8w3hbr)`,
+            }}
+            >
+          </div>
+          <div className="bio">
+            The Panda
+            <br />
+            <div className="bio-bio">
+            The Panda says you are out of matches LUL.
+            </div>
+          </div>
+        </div>
+      </TinderCard>
+);
