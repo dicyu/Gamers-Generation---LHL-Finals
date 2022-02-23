6a6cb8d514420dc2d1b668f97ad68841bee832d8
Dickson Yu <dickson.yu@email.com>
2022-02-22 19:20:04 -0500

    Adjusting css on the pages

--- gamers-generation/src/components/Login.js
+++ gamers-generation/src/components/Login.js
@@ -1,7 +1,6 @@
 // import axios from "axios";
 import React, { useState } from "react";
 import Input from "./Information/Input";
-import Button from "./Button";
 import { useNavigate } from "react-router-dom";
 
 import "../App.scss";
